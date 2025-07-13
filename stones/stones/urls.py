c4039b53e67d4ea85ec3fac432a499ef75d184b3
Владимир Лигатюк <vligatyuk@gmail.com>
2025-07-13 18:49:55 +0200

    removed path for landing

--- stones/stones/urls.py
+++ stones/stones/urls.py
@@ -25,7 +25,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path("", include("landing.urls", namespace="landing")),
+    # path("", include("landing.urls", namespace="landing")),
     path("api/", include("api.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
