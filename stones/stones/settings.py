45d2ae1db4f6572c6ba041f53fe3639f8745b5e6
Ligatiuk Vladimir <107205111+BlackSonoJ@users.noreply.github.com>
2025-10-31 00:38:05 +0300

    Fix ALLOWED_HOSTS syntax in settings.py

--- stones/stones/settings.py
+++ stones/stones/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-)kobhzx2hba$*_^e&%)kt=-02_@@r9&uaw*ttf#&y=$p(9ar&s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
